4858ec23d1f0ba6920f5680f244a02bda427bb3a
Nezamov Stanislav <snezamov@MacBook-Pro-Nezamov.local>
2021-10-28 23:42:48 +0300

    Change func to class

--- pwgen.py
+++ pwgen.py
@@ -10,34 +10,43 @@ if sys.version_info < (3, 6, 0):
     exit(1)
 
 
-dig = False
-dificult = False
+class Password:
+    def __init__(self, password_length: int = 16) -> None:
+        self.password_length = password_length
+        self.include_symbols = "!?@#$%^&*()-+=~_"
+        self.include_own_symbols = ""
+        self.include_numbers = string.digits
+        self.include_lowercase_characters = string.ascii_lowercase
+        self.include_uppercase_characters = string.ascii_uppercase
+        self.exclude_symbols = []  # ( { } [ ] ( ) / \ ' " ` ~ , ; : . < > )
+
+    def add_punctuation_symbols(self):
+        """ Set new include_symbols - !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ """
+        self.include_symbols = string.punctuation
+
+    def set_new_password_length(self, digital: int):
+        """ Set new password length """
+        self.password_length = digital
+
+    def password(self):
+        """ Returns the generated password """
+        password = ""
+        password_kit = self.include_numbers + self.include_lowercase_characters + \
+            self.include_uppercase_characters + \
+            self.include_symbols + self.include_own_symbols
+        for i in range(self.password_length):
+            if i in self.exclude_symbols:
+                continue
+            password += choice(password_kit)
+        return password
 
 
-def password(length=12, special=False):
-    """ Возвращает сгенерированный пароль """
-    password = ""
-    if special:
-        password_kit = string.ascii_letters + string.digits + string.punctuation
-    else:
-        password_kit = string.ascii_letters + string.digits
-    for i in range(length):
-        password += choice(password_kit)
-    return password
-
+my_password = Password()
 
 for i in sys.argv[1:]:
     if not i.startswith("-") and i.isdigit():
-        dig = int(i)
+        my_password.set_new_password_length(int(i))
     if not i.startswith("-") and not i.isdigit():
-        dificult = True
+        my_password.add_punctuation_symbols()
 
-
-if dig and dificult:
-    print(password(dig, dificult))
-elif dig:
-    print(password(dig))
-elif dificult:
-    print(password(special=dificult))
-else:
-    print(password())
+print(my_password.password())
